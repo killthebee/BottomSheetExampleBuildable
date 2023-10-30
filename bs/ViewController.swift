c06c6a0f585743dc1529f04949b21ada88de54da
killthebee <danny.developer@ro.ru>
2023-10-30 14:29:04 +0300

    make example

--- bs/ViewController.swift
+++ bs/ViewController.swift
@@ -1,19 +1,37 @@
-//
-//  ViewController.swift
-//  bs
-//
-//  Created by admin on 30.10.2023.
-//
-
+import SnapKit
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+    
+    private let delegate = BottomSheetTransitioningDelegate(configuration: .default)
+    
+    private lazy var button: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .black
+        button.setTitle("Show bottom sheet", for: .normal)
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .systemBackground
+        
+        view.addSubview(button)
+        button.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalToSuperview().offset(150)
+            $0.width.equalTo(200)
+            $0.height.equalTo(40)
+        }
     }
-
-
+    
+    @objc private func didTapButton() {
+        let vc = ExampleViewController()
+        vc.modalPresentationStyle = .custom
+        vc.transitioningDelegate = delegate
+        present(vc, animated: true)
+    }
 }
 
